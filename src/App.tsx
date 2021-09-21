0329c68bf6cb80185d08fae1e16970b0aabc2030
Văn Khắc Huy <huyvk@vmodev.com>
2021-09-21 12:01:58 +0700

    Read file csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const onChangeFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const text = (e.target as any).result;
+      console.log(text);
+      alert(text);
+    };
+    reader.readAsText((e.target as any).files[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={onChangeFile} />
     </div>
   );
 }
